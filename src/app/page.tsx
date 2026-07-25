ae16c0e798d879d904c9ab9dec378b64acf03520
GENESYS X <antwon.wardlaw@gmail.com>
2026-07-24 21:05:03 -0400

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-8 gap-6">
-      <h1 className="text-6xl font-black">QuoteSnap</h1>
-      <p className="text-zinc-400">Sprint 0 - Foundation Ready</p>
-    </main>
-  );
-}
